93e3420ab84cf6b5533026ecc4e5444b3d620fd2
ZoraizQ <21100130@lums.edu.pk>
2020-06-27 20:30:40 +0500

    fix: rename backend folder

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "firelog.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "virtualfridge.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- virtualfridge/wsgi.py
+++ virtualfridge/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for firelog project.
+WSGI config for virtualfridge project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "firelog.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "virtualfridge.settings")
 
 application = get_wsgi_application()
