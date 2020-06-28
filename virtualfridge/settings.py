e53ad25df77d7ee8543ac2505bb28679937a10b2
ZoraizQ <21100130@lums.edu.pk>
2020-06-29 03:15:34 +0500

    fix: deployment version test

--- virtualfridge/settings.py
+++ virtualfridge/settings.py
@@ -128,5 +128,5 @@ ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = '*'
 
 # Configure Django App for Heroku.
-# import django_heroku
-# django_heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
